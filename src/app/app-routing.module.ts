deffd2698d35ee4c69426e9396cef065a82f6451
Mochi <mochiko@mochi.com>
2020-06-25 17:02:52 -0400

    add routing and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CourseScreenComponent } from './components/course-screen/course-screen.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+import { TopicScreenComponent } from './components/topic-screen/topic-screen.component';
 
-const routes: Routes = [];
+//here's where we add our routes
+const routes: Routes = [
+  {path:'',component:HomeScreenComponent},
+  {path:'Home',component:HomeScreenComponent},
+  {path:'Course',component: CourseScreenComponent},
+  {path:'Topic',component: TopicScreenComponent},
+  {path:'',component:HomeScreenComponent},
+  {path:'**',component:HomeScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
