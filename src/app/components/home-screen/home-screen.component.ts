3e4406c2206715bce9c093feee5a71da7b3599dc
Mochi <mochiko@mochi.com>
2020-06-30 15:17:24 -0400

    redid API, simple and hardcoded with add Course functionality

--- src/app/components/home-screen/home-screen.component.ts
+++ src/app/components/home-screen/home-screen.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-
+import { APIService } from '../../API.service';
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
   styleUrls: ['./home-screen.component.scss']
 })
 export class HomeScreenComponent implements OnInit {
-
-  constructor() { }
+  courses: Array<any>;
+  count: number = 0;
+  profName: String;
+  constructor(private apiservice: APIService) { }
 
   ngOnInit(): void {
+    this.apiservice.ListProfessors().then((evt)=>{
+      if(evt.items == null){
+        this.apiservice.CreateProfessor({
+          professorName:"haku",
+          id:"0",
+          universityName:"UF"
+        }).then((evt)=>{
+          console.log(evt);
+          this.profName=evt.professorName;
+        }).catch((err)=>{
+          console.log(err);
+        })
+      }
+    }).catch((err)=>{
+      console.log(err);
+    })
+    this.apiservice.ListCourses().then((evt)=>{
+      this.courses = evt.items;
+      console.log(evt.items);
+    }).catch((err)=>{
+      console.log(err);
+    });
+
+    this.apiservice.OnCreateCourseListener.subscribe((evt)=>{
+      const data = (evt as any).value.data.onCreateCourse;
+      this.courses =[...this.courses,data];
+    })
+  }
+
+  createCourse(){
+    this.apiservice.CreateCourse({
+      courseName:"A test Course",
+      courseDescription:"TESTING TESTING HAHAHA",
+      professor:"haku",
+      id:this.count.toString()
+    }).then((evt)=>{
+      console.log(evt);
+    });
   }
 
 }
