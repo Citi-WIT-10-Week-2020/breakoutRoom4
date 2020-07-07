0ce3ea8d3c5e702a119f5dba2bd07c4dbae89359
Anjali M <jag@Anjalis-MacBook-Pro-2.local>
2020-07-07 14:44:49 -0500

    updated username

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Amplify, { Auth } from 'aws-amplify';
-
+import { Hub, Logger} from 'aws-amplify';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,31 +11,69 @@ import Amplify, { Auth } from 'aws-amplify';
 
 export class NavBarComponent implements OnInit {
   profName:String;
-  //javascript promises and async programming
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-   Auth.currentUserInfo().then((evt)=>{
+
+    /*
+    Auth.currentUserInfo().then((evt)=>{
       console.log(evt);
       this.profName = evt.username;
     });
-    //console.log(info);
-    //this.profName = info.username;
-    //console.log(info.username);
+
+    this.profName = "";
+    
+    */
+
+    this.displayName();
+
   }
 
-   /*async displayName(){
-    let info = await Auth.currentUserInfo();
-    console.log(info.username);
-    //console.log(info);
-   
-
-  } */
+  // uses Hub from Amplify to listen for sign in. sign up, and sign out
   
+  displayName () {
+
+    const logger = new Logger('My-Logger');
+    console.log(logger);
+    const listener = (data) => {
+      console.log(data);
+      switch (data.payload.event) {
+
+         case 'signIn':
+              this.displayUserName();
+              break;
+          case 'signUp':
+              this.displayUserName();
+             break;
+          case 'signOut':
+             this.profName = "";
+             console.log(this.profName);
+             console.log("sign out");
+             break;
+      }
+    }
+    console.log("in the function");
+    Hub.listen('auth', listener);
+  }
+
+  /// when user signs in or signs out, the displayUserName function is called to 
+  // read the name using currentUserInfo
+  displayUserName() {
+    Auth.currentUserInfo().then((evt)=>{
+      console.log(evt);
+      this.profName = evt.username;
+    });
+
+    console.log("in displayUserName");
+  }
+
+} 
+
+
   
 
-}
+
 
 
 
