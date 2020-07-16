d780f83b0c971c08b22f529fcc37ee28c459cae7
Anjali M <jag@Anjalis-MacBook-Pro-2.local>
2020-07-16 14:08:13 -0500

    topic functionality

--- src/app/components/topic/topic.component.ts
+++ src/app/components/topic/topic.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { APIService } from '../../API.service';
+import { TopicsService } from '../../shared/topics.service';
 
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
-  styleUrls: ['./topic.component.scss']
+  styleUrls: ['./topic.component.scss'],
+
 })
 export class TopicComponent implements OnInit {
 
@@ -12,15 +15,35 @@ export class TopicComponent implements OnInit {
   @Input() professor: String;
   @Input() topicDescription: String;
 
-  constructor() { }
+  constructor(private apiservice: APIService, private topicservice:TopicsService) {}
 
   ngOnInit(): void {
   }
 
   //will be able to edit a certain topic
-  updateTopic() {}
+  deleteTopic() {
+    const myObserver = {
+      next: x => {
+        console.log('Value: ' , x);
+      },
+      error: err => console.error('Observer got an error: ' + err),
+      complete: () => console.log('Observer got a complete notification'),
+    };
+    this.topicservice.deleteTopic(this.topicID.toString()).subscribe(myObserver);
+  }
 
   //will be able to delete a certain topic
-  deleteTopic() {}
+  updateTopic() {
+    const myObserver = {
+      next: x => {
+        console.log('Value: ' , x);
+      },
+      error: err => console.error('Observer got an error: ' + err),
+      complete: () => console.log('Observer got a complete notification'),
+    };
+   // this.topicservice.updateTopic(this.topicObject).subscribe(myObserver);
+  }
+
+
 
 }
