625ba4123f99a2a8657ffc1a16850a9d4b8a8ff5
Caitlin Lambert <clambert23@amherst.edu>
2020-07-16 08:57:51 -0500

    fixed routing for topic screen and prelim help page!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CourseScreenComponent } from './components/course-screen/course-screen.component';
 import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 import { TopicScreenComponent } from './components/topic-screen/topic-screen.component';
+import { HelpScreenComponent } from './components/help-screen/help-screen.component';
 
 //here's where we add our routes
 const routes: Routes = [
   {path:'',component:HomeScreenComponent},
   {path:'course/:id',component: CourseScreenComponent},
-  {path:'topic/:id/:TopicName',component: TopicScreenComponent},
+  {path:'topic/:courseId/:id/:TopicName',component: TopicScreenComponent},
+  {path: 'help', component: HelpScreenComponent},
   {path:'home',component:HomeScreenComponent},
   {path:'**',component:HomeScreenComponent}
 ];
