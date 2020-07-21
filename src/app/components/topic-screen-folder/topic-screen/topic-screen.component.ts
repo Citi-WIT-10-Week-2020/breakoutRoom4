2fa89880151fda3b6d72a2db1728b04f9d090c1d
Caitlin Lambert <clambert23@amherst.edu>
2020-07-21 10:19:30 -0500

    updates to about and topic screen

--- src/app/components/topic-screen-folder/topic-screen/topic-screen.component.ts
+++ src/app/components/topic-screen-folder/topic-screen/topic-screen.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { APIService } from '../../API.service';
+import { APIService } from '../../../API.service';
 
 @Component({
   selector: 'app-topic-screen',
