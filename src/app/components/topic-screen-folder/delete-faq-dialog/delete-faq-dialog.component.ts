f69fa637594304c3e41c7951adc7f4a0f7df1966
Caitlin Lambert <clambert23@amherst.edu>
2020-07-28 14:09:01 -0500

    updates to faq/about

--- src/app/components/topic-screen-folder/delete-faq-dialog/delete-faq-dialog.component.ts
+++ src/app/components/topic-screen-folder/delete-faq-dialog/delete-faq-dialog.component.ts
@@ -1,25 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from "@angular/material/dialog";
 import { Input } from '@angular/core';
+import { FileService } from 'src/app/shared/file.service';
+import { IFile } from 'src/app/shared/file';
 
 @Component({
   selector: 'app-delete-faq-dialog',
   templateUrl: './delete-faq-dialog.component.html',
-  styleUrls: ['./delete-faq-dialog.component.scss']
+  styleUrls: ['./delete-faq-dialog.component.scss'],
+  providers:[FileService]
 })
 export class DeleteFaqDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<DeleteFaqDialogComponent>) { }
+  constructor(public dialogRef: MatDialogRef<DeleteFaqDialogComponent>, private fileservice:FileService) { }
+
+  @Input() id: string;
 
   ngOnInit(): void {
   }
 
-  cancel(): void{
+
+  deleteFaq() {
+    /*const myObserver = {
+      next: x => {
+        console.log('Value: ' , x);
+      },
+      error: err => console.error('Observer got an error: ' + err),
+      complete: () => console.log('Observer got a complete notification'),
+    };*/
+    this.fileservice.deleteFile(this.id);
     this.dialogRef.close();
   }
 
-  deleteFaq() {
-    
+  cancel(): void{
+    this.dialogRef.close();
   }
 
 }
--- src/app/components/topic-screen-folder/update-faq-dialog/update-faq-dialog.component.ts
+++ src/app/components/topic-screen-folder/update-faq-dialog/update-faq-dialog.component.ts
@@ -1,29 +1,34 @@
 import { Component, OnInit, Input, Inject } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-
+import { FileService } from 'src/app/shared/file.service';
+import { IFile } from 'src/app/shared/file';
 
 @Component({
   selector: 'app-update-faq-dialog',
   templateUrl: './update-faq-dialog.component.html',
-  styleUrls: ['./update-faq-dialog.component.scss']
+  styleUrls: ['./update-faq-dialog.component.scss'],
+  providers:[FileService]
 })
 export class UpdateFaqDialogComponent implements OnInit {
 
-  @Input() question: string;
-  @Input() answer: string;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<UpdateFaqDialogComponent>, private fileservice:FileService) { }
+
+  @Input() filename: string;
+  @Input() fileDescription: string;
+
 
   faq: FormGroup;
-  faqObject: any;
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<UpdateFaqDialogComponent>) { }
+  faqObject: IFile;
 
   ngOnInit(): void {
 
-    this.faqObject = {
-      question : this.question,
-      answer : this.answer,
-    }
+    /*this.faqObject = {
+      filename : this.filename,
+      fileDescription : this.fileDescription,
+      filetype: "Question/Answer",
+      id: null,
+    }*/
 
     this.faq = new FormGroup({
       question: new FormControl(''),
@@ -38,8 +43,8 @@ export class UpdateFaqDialogComponent implements OnInit {
 
   cancel() {
     this.dialogRef.close();
-    console.log("Question:",this.question);
-    console.log("Answer:",this.answer);
+ //   console.log("Question:",this.question);
+  //  console.log("Answer:",this.answer);
   }
 
 }
